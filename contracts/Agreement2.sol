439629d6af0d9ad12f224b9a1626d2c7c54dacf3
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-10-06 11:57:14 +0200

    refactor(contracts): refactors Agreement2

--- contracts/Agreement2.sol
+++ contracts/Agreement2.sol
@@ -5,7 +5,6 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {IERC1820Registry} from "@openzeppelin/contracts/interfaces/IERC1820Registry.sol";
 import {Errors} from "./libraries/Errors.sol";
-import "hardhat/console.sol";
 
 contract Agreement2 is Initializable, OwnableUpgradeable {
     mapping(address => uint256) _toClaim;
@@ -17,18 +16,18 @@ contract Agreement2 is Initializable, OwnableUpgradeable {
     event IpfsMultihashChanged(string ipfsMultihash);
     event ClaimForSet(address owner, uint256 amount);
 
-    function initialize(string calldata _ipfsMultihash) external initializer {
-        ipfsMultihash = _ipfsMultihash;
+    function initialize(string calldata ipfsMultihash_) external initializer {
+        ipfsMultihash = ipfsMultihash_;
         __Ownable_init();
     }
 
-    function changeIpfsMultihash(string calldata _ipfsMultihash) external onlyOwner {
-        ipfsMultihash = _ipfsMultihash;
-        emit IpfsMultihashChanged(_ipfsMultihash);
+    function changeIpfsMultihash(string calldata ipfsMultihash_) external onlyOwner {
+        ipfsMultihash = ipfsMultihash_;
+        emit IpfsMultihashChanged(ipfsMultihash_);
     }
 
-    function acceptAndClaim(string calldata _ipfsMultihash) external {
-        if (keccak256(abi.encodePacked((_ipfsMultihash))) != keccak256(abi.encodePacked((ipfsMultihash)))) {
+    function acceptAndClaim(string calldata ipfsMultihash_) external {
+        if (keccak256(abi.encode((ipfsMultihash_))) != keccak256(abi.encode((ipfsMultihash)))) {
             revert Errors.InvalidIpfsMultiHash();
         }
 
@@ -39,65 +38,65 @@ contract Agreement2 is Initializable, OwnableUpgradeable {
             revert Errors.NothingToClaim();
         }
 
-        _acceptAndClaimFor(msgSender, msgSender, amountToClaim, _ipfsMultihash);
+        _acceptAndClaimFor(msgSender, msgSender, amountToClaim, ipfsMultihash_);
     }
 
-    function acceptAndClaimManyOwner(address[] calldata _owners) external onlyOwner {
+    function acceptAndClaimManyOwner(address[] calldata owners) external onlyOwner {
         address msgSender = _msgSender();
-        for (uint256 i = 0; i < _owners.length; i++) {
-            uint256 amountToClaim = getClaimableAmountFor(_owners[i]);
-            _acceptAndClaimFor(_owners[i], msgSender, amountToClaim, "");
+        for (uint256 i = 0; i < owners.length; i++) {
+            uint256 amountToClaim = getClaimableAmountFor(owners[i]);
+            _acceptAndClaimFor(owners[i], msgSender, amountToClaim, "");
         }
     }
 
-    function acceptAndClaimOwner(address _owner) public onlyOwner {
-        uint256 amountToClaim = getClaimableAmountFor(_owner);
-        _acceptAndClaimFor(_owner, _msgSender(), amountToClaim, "");
+    function acceptAndClaimOwner(address owner) external onlyOwner {
+        uint256 amountToClaim = getClaimableAmountFor(owner);
+        _acceptAndClaimFor(owner, _msgSender(), amountToClaim, "");
     }
 
-    function setClaimForMany(address[] calldata _owners, uint256[] calldata _amounts) public onlyOwner {
-        if (_owners.length != _amounts.length) {
+    function setClaimForMany(address[] calldata owners, uint256[] calldata amounts) external onlyOwner {
+        if (owners.length != amounts.length) {
             revert Errors.InvalidLength();
         }
 
-        for (uint256 i = 0; i < _owners.length; i++) {
-            setClaimFor(_owners[i], _amounts[i]);
+        for (uint256 i = 0; i < owners.length; i++) {
+            setClaimFor(owners[i], amounts[i]);
         }
     }
 
-    function setClaimFor(address _owner, uint256 _amount) public onlyOwner {
-        if (_amount == 0) {
+    function setClaimFor(address owner, uint256 amount) public onlyOwner {
+        if (amount == 0) {
             revert Errors.InvalidAmount();
         }
 
-        _toClaim[_owner] = _amount;
-        emit ClaimForSet(_owner, _amount);
+        _toClaim[owner] = amount;
+        emit ClaimForSet(owner, amount);
     }
 
-    function getClaimableAmountFor(address _owner) public view returns (uint256) {
-        return _toClaim[_owner] - _claimed[_owner];
+    function getClaimableAmountFor(address owner) public view returns (uint256) {
+        return _toClaim[owner] - _claimed[owner];
     }
 
-    function getClaimedAmountFor(address _owner) public view returns (uint256) {
-        return _claimed[_owner];
+    function getClaimedAmountFor(address owner) external view returns (uint256) {
+        return _claimed[owner];
     }
 
-    function emergencyWithdraw(uint256 _amount) external onlyOwner {
-        _send(_msgSender(), _amount);
+    function emergencyWithdraw(uint256 amount) external onlyOwner {
+        _send(_msgSender(), amount);
     }
 
-    function _acceptAndClaimFor(address _owner, address _receiver, uint256 _amount, string memory _ipfsMultihash) internal {
-        _claimed[_owner] += _amount;
-        _send(_receiver, _amount);
-        emit AcceptedAndClaimed(_owner, _ipfsMultihash, _amount);
+    function _acceptAndClaimFor(address owner, address _receiver, uint256 amount, string memory ipfsMultihash_) internal {
+        _claimed[owner] += amount;
+        _send(_receiver, amount);
+        emit AcceptedAndClaimed(owner, ipfsMultihash_, amount);
     }
 
-    function _send(address _to, uint256 _amount) internal {
-        (bool sent, ) = _to.call{value: _amount}("");
+    function _send(address _to, uint256 amount) internal {
+        (bool sent, ) = _to.call{value: amount}("");
         if (!sent) revert Errors.FailedToSendEther();
     }
 
-    fallback() external payable {
+    receive() external payable {
         if (_msgSender() != owner()) {
             revert Errors.OnlyOwnerCanDepositToken();
         }
